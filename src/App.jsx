326ab7556ad17037ee993c5242e212464ee221a9
Ondřej Zahradník <ondrabondy@gmail.com>
2023-08-22 09:32:52 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Navbar from './components/Navbar';
+
+function App() {
+  
+  return (
+    <div className="app">
+      <Router>
+        <Navbar />
+        <Routes>
+        <Route path="/" />
+        </Routes>
+      </Router>
+    </div>
+  )
+}
+
+export default App
